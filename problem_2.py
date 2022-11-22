15bd0d6d8a40f16c1e4e3023a90a03b0ddc83eca
kjartanfh19@ru.is <kjartanfh19@ru.is>
2022-11-22 11:21:53 +0000

    byrja á 3

--- problem_2.py
+++ problem_2.py
@@ -4,13 +4,25 @@ x = 0
 y = 0
 z = 6370
 
-def location(theta, phi):
+def location(phi, theta):
     A = rho * sin(phi) * cos(theta) if abs(rho * sin(phi) * cos(theta)) > 1e-10 else 0
     B = rho * sin(phi) * sin(theta) if abs(rho * sin(phi) * sin(theta)) > 1e-10 else 0
     C = rho * cos(phi) if abs(rho * cos(phi)) > 1e-10 else 0
     distance = sqrt(pow((x - A), 2) + pow((y - B), 2) + pow((z - C), 2))
     t = distance / 299792.458
 
-    print('A =', A, 'km\nB =', B, 'km\nC =', C, 'km\nDistance from north pole =', distance, 'km\nt =', t, 'sec')
+    print('A =', A, 'km\nB =', B, 'km\nC =', C, 'km\nDistance from north pole =', distance, 'km\nt =', t, 'sec\n\n')
+    
+    return
 
-location(pi,0.1)
+#Problem 3 starts here:
+corr_phi = [pi/8, pi/6, 3*pi/8, pi/4]
+corr_theta = [-pi/4, pi/2, 2*pi/3, pi/6]
+
+def main():
+    for i in range(len(corr_phi)):
+        location(corr_phi[i], corr_theta[i])
+    return
+
+if __name__ == "__main__":
+    main()
