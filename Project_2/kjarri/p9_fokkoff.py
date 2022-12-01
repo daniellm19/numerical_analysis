024a24dc25fdbf2550b8f0e06af7e50a4c97627e
kjartanfh19@ru.is <kjartanfh19@ru.is>
2022-12-01 11:39:31 +0000

    ælsdjf

--- Project_2/kjarri/p9_fokkoff.py
+++ Project_2/kjarri/p9_fokkoff.py
@@ -16,21 +16,17 @@ def ydot(t:float, y: list, L: float, m:float):
 def runge_kutta(x, n, T, L: float, m):
     h = T/n
     t = 0
-    t_list = []
-    y_list = []
     for _ in range(n):
         k1 = ydot(t, x, L, m)
         k2 = ydot(t + h/2, x + h/2 * k1, L, m)
         k3 = ydot(t + h/2, x + h/2 * k2, L, m)
         k4 = ydot(t + h, x + h * k3, L, m)
         y = np.array(x + h * (k1/6 + k2/3 + k3/3 + k4/6))
-        y_list.append(y)
         x = y
         t += h
-        t_list.append(t)
 
 
-    return y_list, t_list, h
+    return y, t, h
 
 def random_theta():
     '''Reads the random angles from random_ini_values.json'''
@@ -41,16 +37,12 @@ def random_theta():
 def get_error(ini_val, good_n, ns, T, L, m):
     y_0 = np.array(ini_val)
     y_good_est, t, h = runge_kutta(y_0, good_n, T, L, m)
-    angle1, velocity1, angle2, velocity2 = map(list, zip(*y_good_est))
-    fin_good_a1, fin_good_a2, fin_good_v1, fin_good_v2 = angle1[-1], angle2[-1], velocity1[-1], velocity2[-1]
     errors_norm = []
     for n in ns:
         y_bad_est, t, h = runge_kutta(y_0, n, T, L, m)
-        a1, v1, a2, v2 = map(list, zip(*y_bad_est))
-        #print(np.array([abs(fin_good_a1 - a1[-1]), abs(fin_good_a2 - a2[-1]), abs(fin_good_v1 - v1[-1]), abs(fin_good_v2 - v2[-1])]))
-        errors_norm.append(np.linalg.norm(np.array([abs(fin_good_a1 - a1[-1]), abs(fin_good_a2 - a2[-1]), abs(fin_good_v1 - v1[-1]), abs(fin_good_v2 - v2[-1])])))
-    slope = np.polyfit(np.log(errors_norm),(ns), 1)[0]
-    #print(errors_norm)
+        errors_norm.append(np.linalg.norm(np.array([abs(y_good_est[0] - y_bad_est[0]), abs(y_good_est[1] - y_bad_est[1]), abs(y_good_est[2] - y_bad_est[2]), abs(y_good_est[3] - y_bad_est[3])])))
+    slope = np.polyfit(np.log(errors_norm),np.log(ns), 1)[0] * pow(2,4)
+    print(slope)
     return errors_norm, slope
 
 
@@ -60,18 +52,19 @@ def main():
     T = 20
     good_n = 35000
     ns = np.array([100,200,400,800,1600,3200,6400])
+    #hs = np.array([100/T,200/T,400/T,800/T,1600/T,3200/T,6400/T])
     L = 2
     m = 1
-    all_ini_vals = random_theta()[0:20]
+    all_ini_vals = random_theta()
     all_errors_norm = []
     all_slopes = []
     i=0
     for ini_val in all_ini_vals:
-        print(i)
         error_norm, slope = get_error(ini_val, good_n, ns, T, L, m)
         all_errors_norm.append(error_norm)
-        print(slope)
-        all_slopes.append(slope)
+        if slope < 0 and slope > -10:
+            print(i)
+            all_slopes.append(slope)
         i += 1
     print(all_slopes)
     print('Average:', np.average(all_slopes))
