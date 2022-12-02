cd84016b868e51922570bfc19d5585a1d8a3bb49
karisteinn99 <71447465+karisteinn99@users.noreply.github.com>
2022-12-02 13:24:16 +0000

    hello

--- Project_2/AAA - Main/problem_2_3_4.py
+++ Project_2/AAA - Main/problem_2_3_4.py
@@ -2,7 +2,6 @@ import matplotlib.pyplot as plt
 from numpy import sin, cos, pi
 import numpy as np
 import matplotlib.animation as animation
-from collections import deque
 
 # Problem 2 starts here: 
 
@@ -14,16 +13,16 @@ def y_dot(t, y):
     L = 2 # lenght of pendulum in m
     return [y[1], -(g/L)*sin(y[0])]
 
-def Problem_one_euler_method(T, n, z_0):
+def Problem_one_euler_method(T, n, y_0):
     """
     T: interval from [0, T]
     n: number of segmentations on interval [0, T]
-    z_0: initial vector values for angular position and angular speed. [theta, theta_prime]
+    y_0: initial vector values for angular position and angular speed. [theta, theta_prime]
     
     This function uses the euler method to emulate the movement of pendumulum with differential equation from problem one (see equations in eulerstep and y_dot).
     Returns t, theta[0], theta[1] (t = time array, theta[0] = theta position array, theta[1] = theta speed array)
     """
-    z = z_0
+    z = y_0
     h = T/n
     t = [i*h for i in range(0, n)]
     theta = [[], []] # array for the changing values of theta
---
+++ Project_2/AAA - Main/problem_5.py
@@ -0,0 +1,4 @@
+import matplotlib.pyplot as plt
+from numpy import sin, cos, pi
+import numpy as np
+import matplotlib.animation as animation
