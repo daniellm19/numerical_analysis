0e8cc9e3294a420aec512dc8bc138901921d7a4e
karisteinn99 <71447465+karisteinn99@users.noreply.github.com>
2022-12-03 15:16:25 +0000

    problem 11 FINISHED. can´t be done better...

--- Project_2/AAA - Main/problem_11.py
+++ Project_2/AAA - Main/problem_11.py
@@ -48,6 +48,30 @@ def plot(angle: list, angle_error: list, t, title):
         fig.suptitle(title, fontsize=15)
         plt.grid()
         
+def error_list(yn: list, y: list, n):
+    ret_arr = []
+    for i in range(n):
+        ret_arr.append([abs(y[i][0]-yn[i][0]),abs(y[i][1]-yn[i][1]),abs(y[i][2]-yn[i][2]),abs(y[i][3]-yn[i][3])])
+    return ret_arr
+
+def print_the_error(error: list, t):
+    fig = plt.figure(figsize=(12, 7))  
+    plt.subplots_adjust(top=0.9, left=0.06, right=0.96, hspace=0.5, bottom=0.08)
+    fig.suptitle("Difference in angle with starting error 10^(-k) with k ∈ {1,2,3,4,5}")
+    counter = 1
+    for element in error:
+        plt.subplot(3, 2, counter)
+        plt.grid()
+        angle1, velocity1, angle2, velocity2 = map(list, zip(*element))
+        plt.plot(t, angle1, label="Inner pendulum error")
+        plt.plot(t, angle2, label="Outer pendulum error")
+        plt.xlabel('Time [s]',fontsize=7)
+        plt.ylabel('Radians', fontsize=7)
+        plt.legend(loc=2, prop={'size': 6})
+        plt.title(f"Starting with error 10^-({counter}) [rad]", fontsize=9)
+        counter += 1 
+    plt.show()
+        
 def animate_penduli(x_1, y_1, x_2, y_2, ex_1, ey_1, ex_2, ey_2, n, h, k):
     
     fig = plt.figure(figsize=(10, 5))
@@ -72,6 +96,8 @@ def animate_penduli(x_1, y_1, x_2, y_2, ex_1, ey_1, ex_2, ey_2, n, h, k):
     etrace, = ax2.plot([], [], '.-', c='red', lw=0.5, ms=1)
     etime_text = ax2.text(0.05, 0.9, '', transform=ax2.transAxes)
     etrajectory_x, etrajectory_y = deque(maxlen=n), deque(maxlen=n)
+    
+
     
     def animate(i):
         # animating double pendulum
@@ -114,19 +140,34 @@ def animate_penduli(x_1, y_1, x_2, y_2, ex_1, ey_1, ex_2, ey_2, n, h, k):
         fig, animate, len(x_1), interval=h*1000, blit=True, repeat=False)
     plt.show()
     return 
-    
+
+# error table for difference in angle at t = 40 (last value), for different k
+def make_table(error: list):
+    print("Error at T=40")
+    print("\tInner\t\t      Outer")
+    counter = 1
+    for item in error:
+        print(f'K = {counter}:  {item[-1][0]}   {item[-1][2]}')
+        counter += 1
+    print()
 def main():
     T = 40
     n = 500
     L = 2
     m = 1
     k = [1,2,3,4,5]
+    
+    # calculating runge kutta for double pendulum with no error
     y0 = np.array([2*pi/3, 0, pi/6, 0])
     y, t, h = runge_kutta(y0, n, T, L, m)
     angle1, velocity1, angle2, velocity2 = map(list, zip(*y))
+    
+    # calculating runge kutta and errors for all values of k
     diff_y = []
     angle1_error_list = []
     angle2_error_list = []
+    error_difference = []
+    
     
     for num in k:
         y0 = np.array([2*pi/3+error(num), 0, pi/6 + error(num), 0])
@@ -134,32 +175,38 @@ def main():
         diff_y.append([y_n, t_n, h_n])
         angle1_error, velocity1_error, angle2_error, velocity2_error = map(list, zip(*y_n))
         angle1_error_list.append(angle1_error), angle2_error_list.append(angle2_error)
-    
-    plot(angle1, angle1_error_list, t, 'Inner pendulum comparison')
-    plot(angle2, angle2_error_list, t, 'Outer pendulum comparison') 
-    plt.show()
-    
-    compare_k = input('Animate correct double penduli, and compare with the one with error with k = [1, 2, 3, 4, 5]. Choose k: ')
-    if not compare_k.isdigit():
-        print("wrong input, I´m shutting down!")
-        quit()
-    k = int(compare_k)
-    if k < 1 | k > 5:
-        print("wrong input, I´m shutting down!")
-        quit()
-    
-    x_1, y_1 = L * sin(angle1[:]), -L * cos(angle1[:])
-    x_2, y_2 = L * sin(angle2[:]) + x_1, -L * cos(angle2[:]) + y_1
-    ex_1, ey_1 = L * sin(angle1_error_list[k-1][:]), -L * cos(angle1_error_list[k-1][:])
-    ex_2, ey_2 = L * sin(angle2_error_list[k-1][:]) + ex_1, -L * cos(angle2_error_list[k-1][:]) + ey_1
-    animate_penduli(x_1, y_1, x_2, y_2, ex_1, ey_1, ex_2, ey_2, n, h, k)
-    
-   
-    
-    plt.show()
-    
+        y_n_error = error_list(y_n, y, n)
+        error_difference.append(y_n_error)
         
+    # position comparison graph
+    if input("Do you want to see position comparison, shown on a graph? (y/n) ").lower() == 'y':
+        plot(angle1, angle1_error_list, t, 'Inner pendulum comparison')
+        plt.show()
+        plot(angle2, angle2_error_list, t, 'Outer pendulum comparison') 
+        plt.show()
+        
+     # error comparison graph
+    if input("Do you want to see the difference in angle, with starting error 10^(-k) where k = {1, 2, 3, 4, 5}, shown on a graph, as well as getting a table of error difference at T = 40? (y/n) ") == 'y':
+        print_the_error(error_difference, t)
+        make_table(error_difference)
+        
+    # position comparison animation
+    if input("Do you want to see the position comparison, with chosen k value, animated? (y/n) ") == 'y':
+        compare_k = input('Animate correct double penduli, and compare with the one with starting error 10^(-k) where k = {1, 2, 3, 4, 5}. Choose k: ')
+        if not compare_k.isdigit():
+            print("wrong input, I´m shutting down!")
+            quit()
+        k = int(compare_k)
+        if k < 1 | k > 5:
+            print("wrong input, I´m shutting down!")
+            quit()
+        
+        x_1, y_1 = L * sin(angle1[:]), -L * cos(angle1[:])
+        x_2, y_2 = L * sin(angle2[:]) + x_1, -L * cos(angle2[:]) + y_1
+        ex_1, ey_1 = L * sin(angle1_error_list[k-1][:]), -L * cos(angle1_error_list[k-1][:])
+        ex_2, ey_2 = L * sin(angle2_error_list[k-1][:]) + ex_1, -L * cos(angle2_error_list[k-1][:]) + ey_1
+        animate_penduli(x_1, y_1, x_2, y_2, ex_1, ey_1, ex_2, ey_2, n, h, k)
     
-    #angle1, velocity1, angle2, velocity2 = map(list, zip(*y))
 
-main()
+if __name__ == '__main__':
+    main()
