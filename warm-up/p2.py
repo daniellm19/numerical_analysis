c912198abc39a5b886bf9ffa350c534cb269b6a9
kjartanfh19@ru.is <kjartanfh19@ru.is>
2022-12-05 21:59:00 +0000

    lsdjkf

--- warm-up/p2.py
+++ warm-up/p2.py
@@ -1,26 +1,30 @@
 import numpy as np
 import matplotlib.pyplot as plt
 
+
 def nlbvpfd(inter, bv, n):
     [a,b] = inter; [ya,yb] = bv
     h = (b-a)/(n+1)
-    w = np.ones(n)				# Initial guess for Newton
-    for _ in range(20):			# Newton
-        w -= np.linalg.solve(jac(w, h), f(w, h))
+    w = np.zeros(n)				# Initial guess for Newton
+    old_w = w + 2 * 1e-5
+    while np.linalg.norm(w-old_w, np.inf)>1e-5:	# Newton
+        old_w = w
         print(w)
+        s = - np.linalg.solve(jac(w, h), f(w, h))
+        w += s
 
 
     plt.plot( np.linspace(a,b,n+2), np.hstack((ya,w,yb)), linewidth=2)
     return w
 
 def f(w, h):
-	n = len(w)
-	y = np.zeros(n)
-	y[0] = 1
-	y[-1] = -1
-	for i in range(1,n-1):
-		y[i] =  w[i-1] - w[i] * (2 + pow(h, 2)) + w[i+1]
-	return y
+    n = len(w)
+    y = np.zeros(n)
+    y[0] = 1
+    y[-1] = -1
+    for i in range(1,n-1):
+        y[i] =  w[i-1] - w[i] * (2 + pow(h, 2)) + w[i+1]
+    return y
 
 def jac(w, h):
 	n = len(w)
@@ -37,5 +41,5 @@ a = 0
 b = 1
 w = nlbvpfd([a,b],[1,-1],  n)
 print(w)
-print('Actual solution:', -0.5820 * pow(np.e, 1) + 1.5820 * pow(np.e, -1))
+
 plt.show()
